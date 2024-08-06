29b307bc4fc2023771ac7ee29b72823024d0fc87
unknown <montty666@gmail.com>
2024-08-07 02:23:44 +0700

    simple player

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
-package main
+package music
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-func helloworld() string {
-	return "Hello World!!"
+	"github.com/gopxl/beep"
+
+	"github.com/gopxl/beep/mp3"
+	"github.com/gopxl/beep/speaker"
+)
+
+func logg(txt string) {
+	fmt.Println(txt)
 }
 
-func main() {
-	fmt.Println(helloworld())
+func PlayMusic() {
+	logg("start")
+	f, err := os.Open("./caravans.mp3")
+	if err != nil {
+		logg("err1")
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		logg("error")
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
 }
